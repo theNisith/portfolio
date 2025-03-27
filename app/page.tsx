17d847da5dac6b3e70136183a3581330267ae8fb
theNisith <nisithdhar127@gmail.com>
2025-03-27 16:32:46 +0530

    Initial commit with Next.js Setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-blac">
+      <div>
+        <h1>Hello, Nisith !</h1>
+      </div>
+    </main>
+  );
+}
