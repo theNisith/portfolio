fe44cfc70e17fee64c21fec5c71816f3cf23cd9e
theNisith <nisithdhar127@gmail.com>
2025-03-27 20:07:02 +0530

    including aceternity components to the UI

---
+++ components/Hero.tsx
@@ -0,0 +1,50 @@
+
+
+
+import { Spotlight } from "./ui/Spotlight";
+
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+ 
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       relative flex items-center justify-center"
+      >
+        
+        <div
+          
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
+
+      <div className="flex justify-center">
+        <div className='max-w-[89vw]'>
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+               Dynamic Web Magic with Next.js 
+            </h2>
+        </div>
+        
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Hero;
